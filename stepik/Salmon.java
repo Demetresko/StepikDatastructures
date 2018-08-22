970f15c9f46f7ec0ce596496ce27f3d97380916b
Dimitriy <kwanmitya@gmail.com>
2018-08-23 01:14:57 +0600

    Another example

---
+++ stepik/Salmon.java
@@ -0,0 +1,26 @@
+package stepik;
+
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class Salmon{
+
+    static final int var = 0;
+
+    static {var = 0;}
+
+    static{var = 10;}
+    public static void main(String[] args) {
+        boolean[] dsdd = {true,false};
+
+        System.out.println( howMany(true, new boolean[]{true, false}));
+    }
+
+    static int howMany(boolean b, boolean[] array){
+        return  array.length;
+    }
+
+
+
+}
