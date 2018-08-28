4c7b934b3c26cb005a5820656a737cc6bfe17b25
Dimitriy <kwanmitya@gmail.com>
2018-08-28 23:46:45 +0600

    Abstract Interface

--- javaCore/InterfaceExample.java
+++ javaCore/InterfaceExample.java
@@ -1,4 +1,24 @@
 package javaCore;
 
-public class InterfaceExample {
+public abstract   interface InterfaceExample {
+    public static final int ANOTHER_CONSTANT = 0;
+
+
+
+
+    abstract int Method();
 }
+
+
+class Check implements InterfaceExample{
+    public static void main(String[] args) {
+        System.out.println( ANOTHER_CONSTANT+ " This compiler definitely " +
+                               " knows something about life");
+    }
+
+    @Override
+    public int Method() {
+        return  129;
+    }
+}
+
