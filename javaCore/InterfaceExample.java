bd0fe9471de0e4d64fc048e69a03cad3e351c2f0
Dimitriy <kwanmitya@gmail.com>
2018-08-30 19:55:51 +0600

    Inheritance in Interfaces and Abstract Classes

--- javaCore/InterfaceExample.java
+++ javaCore/InterfaceExample.java
@@ -2,23 +2,26 @@ package javaCore;
 
 public abstract   interface InterfaceExample {
     public static final int ANOTHER_CONSTANT = 0;
-
-
-
-
     abstract int Method();
 }
 
+interface AnotherInterface {
+    boolean Second();
+}
 
-class Check implements InterfaceExample{
+abstract class AbstractClass implements InterfaceExample, AnotherInterface{
+
+}
+
+
+
+
+class Check extends AbstractClass{  // Compile error must implement methos from AbstractClass ( inherited form both interfaces)
     public static void main(String[] args) {
         System.out.println( ANOTHER_CONSTANT+ " This compiler definitely " +
                                " knows something about life");
     }
 
-    @Override
-    public int Method() {
-        return  129;
-    }
+
 }
 
