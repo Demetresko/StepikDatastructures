7078ae6e3f86e01ea7f4838b4f6144347777ca65
Dimitriy <kwanmitya@gmail.com>
2018-08-30 20:52:00 +0600

    Clashes of two default methods

--- javaCore/InterfaceExample.java
+++ javaCore/InterfaceExample.java
@@ -1,27 +1,21 @@
 package javaCore;
 
-public abstract   interface InterfaceExample {
-    public static final int ANOTHER_CONSTANT = 0;
-    abstract int Method();
-}
-
-interface AnotherInterface {
-    boolean Second();
-}
-
-abstract class AbstractClass implements InterfaceExample, AnotherInterface{
-
-}
-
-
-
-
-class Check extends AbstractClass{  // Compile error must implement methos from AbstractClass ( inherited form both interfaces)
-    public static void main(String[] args) {
-        System.out.println( ANOTHER_CONSTANT+ " This compiler definitely " +
-                               " knows something about life");
+interface Walk{
+    public  default int getSpeed(){
+        return 5;
     }
-
-
 }
 
+interface Run {
+    public default int getSpeed(){
+        return 10;
+    }
+}
+
+
+ class Cat implements Run,Walk{ // INHERITS UNRELATED DEFAULTS FOR...
+
+     public static void main(String[] args) {
+         System.out.println( new Cat().getSpeed());
+     }
+}
